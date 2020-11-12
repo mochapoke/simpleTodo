90986793d5592061b2c7af0845424aefc8fd1149
mochapoke <71932072+mochapoke@users.noreply.github.com>
2020-11-13 02:14:37 +0900

    버튼 available
    삭제버튼, 입력버튼 작동 구현

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -6,12 +6,19 @@ import {
   View,
   Text,
   StatusBar,
+  KeyboardAvoidingView,
 } from 'react-native';
+import { StyledSheet, Platform } from 'react-native'
 import styled from 'styled-components/native';
+import _ from 'lodash';
 
 const Container = styled.SafeAreaView`
   flex:1;
 `;
+const KeyboardAvoidView = styled.KeyboardAvoidingView`
+  flex: 1;
+`;
+
 const Contents = styled.ScrollView`
   flex:1;
   padding: 8px 25px;
@@ -33,27 +40,69 @@ const InputContainer = styled.View`
 const Input = styled.TextInput`
   border: 1px solid #e5e5e5;
   flex: 1
-`; // 최대크기 
+`; 
 const Button = styled.Button`
 `;
 
 
 export default function App(){
+  const [list, setList] = React.useState([
+    { id: '1', todo: ' 할  일1 '},
+    { id: '2', todo: ' 할  일2 '},
+  ]); // useState안에는 기본값
+  const [inputTodo, setInputTodo] = React.useState('');
 
   return(
+    // 크게 2가지 리턴 가능
+    // components, 컴포넌트로 이루어진 배열
     <Container> 
+      <KeyboardAvoidView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>      
       <Contents>
-        <TodoItem> 
+        {list.map(item=>{
+          return(          
+        <TodoItem key={item.id}> 
           <TodoItemText> 
-            할일 목록 표시
+            {item.todo}
           </TodoItemText>
-          <TodoItemButton title='삭제' onPress={ () => {} } />
+          <TodoItemButton 
+            title='삭제' 
+            onPress={ () => {
+              setList(_.reject(list, elem => elem.id === item.id ));
+            } } 
+            />
         </TodoItem>
+          )
+          })}
+     
       </Contents>
       <InputContainer>
-        <Input></Input>
-        <Button title='전송' onPress={ ()=>{} } />
+        < Input value={inputTodo} 
+          onChangeText={ value => setInputTodo(value)} />
+        <Button 
+          title='입력' 
+          onPress={ ()=>{
+            // 원본 배열을 수정하는 push는 리액트에서 사용 불가
+            // inputTodo.push({...});
+            if (inputTodo === ''){
+              return; // 아무 동작 안하기
+            }
+            const newItem = {
+              id: new Date().getTime().toString(), // 유니크한 id 값 필요
+              todo: inputTodo,
+            };
+            setList( [
+              ...list, // 기존 todo
+              newItem, // 추가 todo
+            ] );
+            setInputTodo(''); // 비어지도록!
+          } }
+          color='#636e72'/>
       </InputContainer>
+      
+      </KeyboardAvoidView>
     </Container>
   );
 }
+
+
+
